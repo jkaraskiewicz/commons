77825f3a32a3f886b3a599ec6ad926be4d487519
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-03-18 22:22:23 +0100

    0.1.2

--- src/traits/vec_traits.rs
+++ src/traits/vec_traits.rs
@@ -1,9 +1,9 @@
 pub trait Contains<T: Eq> {
-    fn has(&self, item: T) -> bool;
+    fn has(&self, item: &T) -> bool;
 }
 
 impl<T: Eq> Contains<T> for Vec<T> {
-    fn has(&self, item: T) -> bool {
-        self.iter().find(|element| **element == item).is_some()
+    fn has(&self, item: &T) -> bool {
+        self.iter().find(|element| *element == item).is_some()
     }
 }
