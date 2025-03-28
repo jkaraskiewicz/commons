b7a659893f156e87e1fc93479998963d688e04bb
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-03-28 16:32:00 +0100

    0.1.5

--- src/utils/file_util.rs
+++ src/utils/file_util.rs
@@ -2,17 +2,17 @@ use crate::types::errors::CommonsError;
 use std::{
     fs::{self, File},
     io::{Read, Write},
-    path::PathBuf,
+    path::Path,
 };
 
-pub fn read_file(path: &PathBuf) -> Result<String, CommonsError> {
+pub fn read_file<P: AsRef<Path>>(path: P) -> Result<String, CommonsError> {
     let mut file = File::open(path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents)
 }
 
-pub fn write_file(path: &PathBuf, content: &str) -> Result<(), CommonsError> {
+pub fn write_file<P: AsRef<Path>>(path: P, content: &str) -> Result<(), CommonsError> {
     let mut file = std::fs::OpenOptions::new()
         .write(true)
         .truncate(true)
@@ -23,13 +23,13 @@ pub fn write_file(path: &PathBuf, content: &str) -> Result<(), CommonsError> {
     Ok(())
 }
 
-pub fn exists_directory(path: &PathBuf) -> bool {
+pub fn exists_directory<P: AsRef<Path>>(path: P) -> bool {
     fs::metadata(path)
         .map(|metadata| metadata.is_dir())
         .unwrap_or(false)
 }
 
-pub fn exists_file(path: &PathBuf) -> bool {
+pub fn exists_file<P: AsRef<Path>>(path: P) -> bool {
     fs::metadata(path)
         .map(|metadata| metadata.is_file())
         .unwrap_or(false)
