97a97e333363dc7dbd57860cf69c9cf3327aae9f
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-04-02 00:03:15 +0200

    Update commons

--- src/traits/result.rs
+++ src/traits/result.rs
@@ -1,14 +1,12 @@
-use std::error::Error;
-
 /// A trait defining a conversion from any type to a Result
-pub trait ToResult<E: Error> {
+pub trait ToResult<E> {
     type Source;
     fn to_result(&self, error_fn: impl FnOnce() -> E) -> Result<Self::Source, E>;
 }
 
 /// Implementation for bool
 /// Returns Err when input is false, and Ok otherwise
-impl<E: Error> ToResult<E> for bool {
+impl<E> ToResult<E> for bool {
     type Source = bool;
 
     fn to_result(&self, error_fn: impl FnOnce() -> E) -> Result<Self::Source, E> {
