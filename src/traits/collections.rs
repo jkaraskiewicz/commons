ce4af82c6047b78bdbc187e00787648f7705ba21
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-04-06 16:50:52 +0200

    0.2.3

--- src/traits/collections.rs
+++ src/traits/collections.rs
@@ -8,6 +8,11 @@ pub trait ContainsPredicate<T> {
     fn contains(&self, predicate: impl Fn(&T) -> bool) -> bool;
 }
 
+/// Returns the first element from a collection based on a predicate
+pub trait FirstItemPredicate<T> {
+    fn first(&self, predicate: impl Fn(&T) -> bool) -> Option<&T>;
+}
+
 /// Implementation for Vec
 impl<T: Eq> Contains<T> for Vec<T> {
     fn contains(&self, item: &T) -> bool {
@@ -18,5 +23,11 @@ impl<T: Eq> Contains<T> for Vec<T> {
 impl<T> ContainsPredicate<T> for Vec<T> {
     fn contains(&self, predicate: impl Fn(&T) -> bool) -> bool {
         self.iter().any(predicate)
+    }
+}
+
+impl<T> FirstItemPredicate<T> for Vec<T> {
+    fn first(&self, predicate: impl Fn(&T) -> bool) -> Option<&T> {
+        self.iter().find(|&item| predicate(item))
     }
 }
