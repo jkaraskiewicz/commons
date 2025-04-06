7466984612e467f05921cfa273d3f987ff5b45da
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-04-06 16:42:46 +0200

    0.2.2

--- src/traits/collections.rs
+++ src/traits/collections.rs
@@ -1,11 +1,22 @@
 /// A trait defining whether a collection contains an element
 pub trait Contains<T: Eq> {
-    fn has(&self, item: &T) -> bool;
+    fn contains(&self, item: &T) -> bool;
+}
+
+/// A trait defining whether a collection contains an element based on a predicate
+pub trait ContainsPredicate<T> {
+    fn contains(&self, predicate: impl Fn(&T) -> bool) -> bool;
 }
 
 /// Implementation for Vec
 impl<T: Eq> Contains<T> for Vec<T> {
-    fn has(&self, item: &T) -> bool {
+    fn contains(&self, item: &T) -> bool {
         self.iter().any(|element| element == item)
+    }
+}
+
+impl<T> ContainsPredicate<T> for Vec<T> {
+    fn contains(&self, predicate: impl Fn(&T) -> bool) -> bool {
+        self.iter().any(predicate)
     }
 }
